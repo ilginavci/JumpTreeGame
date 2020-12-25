1c02fe5fd65d2aa34461219b9d149e16e2e40475
Ilgın <52226386+ilginavci@users.noreply.github.com>
2020-12-26 01:01:28 +0300

    spritelar eklendi

---
+++ JumpTree/Assets/Scripts/SceneDrag.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneOneCamera : MonoBehaviour
+{
+    bool upPointerDown = false;
+    bool downPointerDown = false;
+    public float power;
+    private void FixedUpdate()
+    {
+        if(upPointerDown)
+        {
+            transform.Translate(Vector3.up * power * Time.fixedDeltaTime);
+        }
+        if(downPointerDown)
+        {
+            transform.Translate(Vector3.back * power * Time.fixedDeltaTime);
+        }
+    }
+    public void UpButtonDown()
+    {
+
+    }
+    public void UpButtonUp()
+    {
+
+    }
+    public void DownButtonDown()
+    {
+
+    }
+    public void DownButtonUp()
+    {
+
+    }
+}
