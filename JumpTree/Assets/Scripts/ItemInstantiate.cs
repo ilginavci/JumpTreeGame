45f7fecb740c07cb4d52603a8b07ce3fdec17a18
Ilgın <52226386+ilginavci@users.noreply.github.com>
2020-12-26 05:32:04 +0300

    Ağaç ekleme hala denemede

---
+++ JumpTree/Assets/Scripts/ItemInstantiate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemInstantiate : MonoBehaviour
+{
+    public Transform Cam;
+    public GameObject item;
+    public GameObject particle;
+    bool oneTime = true;
+    GameObject tree1;
+    public void OnDrag()
+    {
+        Vector3 cam = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (oneTime)
+        {
+            tree1 = Instantiate(item, new Vector3(cam.x, cam.y, transform.position.z), Quaternion.identity);
+            oneTime = false;
+        }
+        tree1.transform.position = new Vector3(cam.x, cam.y, transform.position.z);
+    }
+}
--- JumpTree/Assets/Scripts/SceneDrag.cs
+++ JumpTree/Assets/Scripts/SceneDrag.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SceneOneCamera : MonoBehaviour
+public class SceneDrag : MonoBehaviour
 {
     bool upPointerDown = false;
     bool downPointerDown = false;
@@ -13,25 +13,25 @@ public class SceneOneCamera : MonoBehaviour
         {
             transform.Translate(Vector3.up * power * Time.fixedDeltaTime);
         }
-        if(downPointerDown)
+        if(downPointerDown && transform.position.y >= 0 )
         {
-            transform.Translate(Vector3.back * power * Time.fixedDeltaTime);
+            transform.Translate(Vector3.up * (-power) * Time.fixedDeltaTime);
         }
     }
     public void UpButtonDown()
     {
-
+        upPointerDown = true;
     }
     public void UpButtonUp()
     {
-
+        upPointerDown = false;
     }
     public void DownButtonDown()
     {
-
+        downPointerDown = true;
     }
     public void DownButtonUp()
     {
-
+        downPointerDown = false;
     }
 }
